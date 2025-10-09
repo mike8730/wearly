c966aa33f643bd75f0662d0b95f9ad6502ea23b4
mike8730 <razerrush5v@gmail.com>
2025-10-09 21:36:03 +0900

    ストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :set_user, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def basic_auth
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def set_user
     @user = resource
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                       keys:[:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date] )
   end
 end
