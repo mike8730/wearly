c9d186e73a7eeb2475475ce56057a69fbda44e50
mike8730 <razerrush5v@gmail.com>
2025-10-09 21:09:33 +0900

    マイグレーションファイルの編集・実行

--- db/migrate/20251009115808_devise_create_users.rb
+++ db/migrate/20251009115808_devise_create_users.rb
@@ -1,11 +1,20 @@
-# frozen_string_literal: true
+## frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :nickname,           null:false
+      t.string  :last_name,          null: false
+      t.string  :last_name_kana,     null: false
+      t.string  :first_name,         null: false
+      t.string  :first_name_kana,    null: false
+      t.date    :birth_date,         null: false
+      t.integer :role,               null: false, default: 0
+
+
 
       ## Recoverable
       t.string   :reset_password_token
