8d8b622ce88940c6c43b34f9a0022866a756efbf
mike8730 <razerrush5v@gmail.com>
2025-10-09 21:33:46 +0900

    コントローラーの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :set_user, if: :devise_controller?
 
   private
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
        username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def set_user
+    @user = resource
   end
 end
