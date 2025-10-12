f7f6585f780a6e616a4197b56011eed2175f32c4
mike8730 <razerrush5v@gmail.com>
2025-10-12 19:57:34 +0900

    item_variantモデルと画像にアソシエーションを追加

--- app/models/item_variant.rb
+++ app/models/item_variant.rb
@@ -4,7 +4,8 @@ class ItemVariant < ApplicationRecord
   validates :color_id, presence: true
   validates :stock_quantity, presence: true
   validates :price, presence: true
-
+  
+  has_many_attached :images
   belongs_to :item
   belongs_to :size
   belongs_to :color
