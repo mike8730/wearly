a10382d6f199447119b096d46a4f3687061c4890
mike8730 <razerrush5v@gmail.com>
2025-12-10 22:47:57 +0900

    商品購入ページのリンク作成・コントローラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_10_132959) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_10_133009) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,6 +65,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_10_132959) do
     t.text "description", null: false
     t.integer "category_id", null: false
     t.integer "gender_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
