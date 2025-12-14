78063fdfdb2a49898348bd1b8fef0c7c5780bce8
mike8730 <razerrush5v@gmail.com>
2025-12-14 23:18:35 +0900

    shipping_addressモデルにバリデーションとアソシエーションを追加

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,10 @@
 class ShippingAddress < ApplicationRecord
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+  validates :prefecture_id, presence: true, numericality: { other_than: 0}
+  
+  belongs_to :order
+  belongs_to_active_hash :prefecture
 end
