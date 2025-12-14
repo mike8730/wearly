8138ab02665d1b39b82c39f659163ca4e0a4a000
mike8730 <razerrush5v@gmail.com>
2025-12-14 22:48:53 +0900

    Orderモデルにバリデーションとアソシエーションを追加

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,6 @@
 class OrderItem < ApplicationRecord
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  
+  belongs_to :order
+  belongs_to :item_variant
 end
