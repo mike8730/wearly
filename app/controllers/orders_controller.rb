5843a8bc401a6ac61bb82672dfb107119d746d70
mike8730 <razerrush5v@gmail.com>
2025-12-18 23:00:53 +0900

    ordersコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
   def new
-    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(order_form_params)
+    if @order_form.save
+      redirect_to root_path, notice: "注文が完了しました。"
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_form_params
+    params.require(:order_form).permit(
+      :postal_code,
+      :city,
+      :address,
+      :phone_number,
+      :prefecture_id,
+      order_items_attributes: [:item_variant_id, :quantity]
+    )
   end
 end
