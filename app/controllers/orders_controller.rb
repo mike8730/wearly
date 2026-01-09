2d1e3e83ec61b58aa90fe77d0c646826fe29c11b
mike8730 <razerrush5v@gmail.com>
2026-01-09 15:28:39 +0900

    OrderForm を単品購入＋カート方式両対応に拡張

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
   def new
-    @order_form = OrderForm.new
     @item_variant = ItemVariant.find(params[:variant_id])
+    @order_form = OrderForm.new(
+      user_id: current_user.id,
+      item_variant_id: @item_variant.id
+    )
     @item = @item_variant.item
   end
 
@@ -10,6 +13,8 @@ class OrdersController < ApplicationController
     if @order_form.save
       redirect_to root_path, notice: "注文が完了しました。"
     else 
+      @item_variant = ItemVariant.find(order_form_params[:item_variant_id])
+      @item = @item_variant.item
       render :new, status: :unprocessable_entity
     end
   end
@@ -17,12 +22,13 @@ class OrdersController < ApplicationController
   private
   def order_form_params
     params.require(:order_form).permit(
+      :item_variant_id,
       :postal_code,
       :city,
       :address,
       :phone_number,
       :prefecture_id,
       order_items_attributes: [:item_variant_id, :quantity]
-    )
+    ).merge(user_id: current_user.id)
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -4,18 +4,21 @@ class OrderForm
 
   attr_accessor :postal_code, :city, :address, :phone_number, :prefecture_id
   attr_accessor :order_items_attributes
+  attr_accessor :user_id, :item_variant_id
   
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :city, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ } 
   validates :prefecture_id, presence: true, numericality: { other_than: 0 }
+  validates :user_id, presence: true
+  validates :item_variant_id, presence: true
 
   def save
     return false unless valid?
 
     ActiveRecord::Base.transaction do
-      order = Order.create!
+      order = Order.create!(user_id: user_id)
 
       ShippingAddress.create!(
         order: order,
@@ -25,12 +28,20 @@ class OrderForm
         phone_number: phone_number,
         prefecture_id: prefecture_id
       )
-
-      order_items_attributes.each do |item_attr|
+      
+      if order_items_attributes.present?
+        Array(order_items_attributes).each do |item_attr|
+          OrderItem.create!(
+            order: order,
+            item_variant_id: item_attr[:item_variant_id],
+            quantity: item_attr[:quantity]
+          )
+        end
+      else
         OrderItem.create!(
           order: order,
-          item_variant_id: item_attr[:item_variant_id],
-          quantity: item_attr[:quantity]
+          item_variant_id: item_variant_id,
+          quantity: 1
         )
       end
     end
