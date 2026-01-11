861526156596030ec2ff575e0f12bcbf14224a3d
mike8730 <razerrush5v@gmail.com>
2026-01-11 17:34:02 +0900

    CartItemsController に商品追加・数量更新の処理を実装

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,17 @@
+class CartItemsController < ApplicationController
+  def create
+    @cart = current_user.cart || current_user.create_cart
+    item_variant = ItemVariant.find(params[:item_variant_id])
+
+    @cart_item = @cart.cart_items.find_by(item_variant_id: item_variant.id)
+
+    if @cart_item
+      @cart_item.quantity += 1
+      @cart_item.save
+    else
+      @cart.cart_items.create(item_variant: item_variant, quantity: 1)
+    end
+
+    redirect_to carts_path, notice: "カートに追加しました"
+  end
+end
---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,6 @@
+class CartsController < ApplicationController
+  def index
+    @cart = current_user.cart || current_user.create_cart
+    @cart_items = @cart.cart_items.includes(:item_variant)
+  end
+end
