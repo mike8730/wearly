2817552f00692644c4c658e8949d6e2efcd37eb2
mike8730 <razerrush5v@gmail.com>
2026-01-29 16:43:41 +0900

    出品フォームの構造を修正し、バリエーションのネストを正しく設定

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,4 +2,10 @@ class Color < ApplicationRecord
   validates :name, presence: true
   has_many :item_variants
   has_many_attached :images
+
+  validate :must_have_at_least_one_image, on: :create
+
+  def must_have_at_least_one_image
+    errors.add(:images, "を1枚以上登録してください") unless images.attached?
+  end
 end
