856683d9cd4e936c038ca98d290015022a2f0841
mike8730 <razerrush5v@gmail.com>
2026-05-13 09:10:50 +0900

    KOMOJUのMultiJsonエラーを解消するためmulti_jsonを追加し読み込みを修正

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require "multi_json"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
