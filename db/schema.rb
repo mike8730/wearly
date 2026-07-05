99a4de57e8381b0e3c50bdfd79cbe6df36c439b7
mike8730 <razerrush5v@gmail.com>
2026-07-05 16:45:00 +0900

    DB移行用のProductCategoryテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_21_102228) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_05_072909) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -141,6 +141,12 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_21_102228) do
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_pending_orders_on_session_id", unique: true
     t.index ["user_id"], name: "index_pending_orders_on_user_id"
+  end
+
+  create_table "product_categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shipping_addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
