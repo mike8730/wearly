4c8221df3ec2bfd1d1cedb6d7fe09aa428c6bcd8
mike8730 <razerrush5v@gmail.com>
2026-07-17 11:54:43 +0900

    product_categoriesコントローラーにshowアクションを追加

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -2,4 +2,15 @@ class ProductCategoriesController < ApplicationController
   def index
     @product_categories = ProductCategory.all
   end
+
+  def show
+    @product_category = ProductCategory
+                          .includes(
+                            items: [:item_colors, :item_variants, :favorites],
+                            item_colors: { images_attachments: :blob }
+                          )
+                          .find(params[:id])
+  
+    @category_items = @product_category.items
+  end
 end
