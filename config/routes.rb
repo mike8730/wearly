44b9c3c82ce3185069c8fdd8d58dba03c445a23a
mike8730 <razerrush5v@gmail.com>
2026-08-24 15:31:25 +0900

    コーディネートスライダーのレイアウト崩れを修正し、CSS/JS/ビューを統合調整

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,5 @@ Rails.application.routes.draw do
 
   resources :product_categories, only: [:index, :show]
 
-  resources :coordinates, only: [:new, :create, :index]
+  resources :coordinates, only: [:new, :create, :index, :show]
 end
